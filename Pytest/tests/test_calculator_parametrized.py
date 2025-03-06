36ef28e8f12607806921b4136052216f5ee1c858
DanielDubnicky <113416260+DanielDubnicky@users.noreply.github.com>
2025-03-06 12:29:03 +0100

    parametrized_done

--- Pytest/tests/test_calculator_parametrized.py
+++ Pytest/tests/test_calculator_parametrized.py
@@ -24,4 +24,36 @@ def test_add_2():
 )
 
 def test_add_parametrized(a, b, exptected):
+    assert calculator.add(a, b) == exptected
+
+def test_add_wrong_parametrized(a, b, exptected):
     assert calculator.add_wrong(a, b) == exptected
+
+@pytest.mark.parametrize(
+    "x, y, exptected",
+    [
+        (1, 2, 2),
+        (0, 5, 0),
+        (-2, 9, -18),
+        (5, -8, -40),
+    ],
+)
+
+def test_multiply_parametrized(x, y, exptected):
+    assert calculator.multiply(x, y) == exptected
+
+def test_multiply_wrong_parametrized(x, y, exptected):
+    assert calculator.multiply_wrong(x, y) == exptected
+
+@pytest.mark.parametrize(
+    "c, d, exptected",
+    [
+        (2, 2, 1),
+        (0, 5, 0),
+        (6, 2, 3),
+        (9, 3, 3),
+    ],
+)
+
+def test_subtract_parametrized(c, d, exptected):
+    assert calculator.subtract(c, d) == exptected
