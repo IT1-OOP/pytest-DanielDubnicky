eb5a970b9475ea58dbda463b32c802997959f944
DanielDubnicky <113416260+DanielDubnicky@users.noreply.github.com>
2025-03-06 12:12:57 +0100

    .

--- Pytest/tests/test_calculator_parametrized.py
+++ Pytest/tests/test_calculator_parametrized.py
@@ -3,18 +3,18 @@ from src import calculator
 
 def test_add():
     assert calculator.add(1,2) == 3
-    assert calculator.add(0,5) == 5
+    assert calculator.add(0, 5) == 5
     assert calculator.add(-2,9) == 7
     assert calculator.add(5, -8) == -3
 
 def test_add_2():
     assert calculator.add_wrong(1,2) == 3
-    assert calculator.add_wrong(0,5) == 5
+    assert calculator.add_wrong(0, 5) == 5
     assert calculator.add_wrong(-2,9) == 7
-    assert calculator.add_wrong(0,0) == 0
+    assert calculator.add_wrong(0, 0) == 0
 
 @pytest.mark.parametrize(
-    "a, b, expected",
+    "a, b, exptected",
     [
         (1, 2, 3),
         (0, 5, 5),
@@ -22,8 +22,6 @@ def test_add_2():
         (5, -8, -3),
     ],
 )
-def test_add_parametrized(a, b, expected):
-    assert calculator.add(a, b) == expected
 
-
-
+def test_add_parametrized(a, b, exptected):
+    assert calculator.add_wrong(a, b) == exptected
